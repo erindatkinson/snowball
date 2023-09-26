3638e013bb74c7901f97ed58b7bba2c63bf200f2
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2023-09-26 15:04:19 -0600

    add service config to parser
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>

--- packages/config/config.py
+++ packages/config/config.py
@@ -2,8 +2,9 @@ from configparser import ConfigParser
 from sys import exit
 import logging
 
+services = {}
 
-def init(config)->(dict|Exception):
+def init(config:str)->(None|Exception):
    parser = ConfigParser()
    with open(config) as fp_in:
     parser.readfp(fp_in)
@@ -11,6 +12,21 @@ def init(config)->(dict|Exception):
     name = parser.get("core", "name", fallback="snowball")
     level = parser.get("core", "log_level", fallback="WARN").upper()
     logging.basicConfig(level=logging.getLevelNamesMapping()[level])
+   
+   hasService = False
+   if "discord" in parser.sections():
+     services["discord"] = parser["discord"]
+     hasService = True
+
+   # adding for future integration
+   if "slack" in parser.sections():
+     services["slack"] = parser["slack"]
+     hasService = True
+
+   if not hasService:
+     raise Exception("no service defined, please add a discord or slack section to your config")
+   
+   
 
 
 
