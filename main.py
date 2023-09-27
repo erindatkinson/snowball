e2da0323165e69691df4b92803b9cd32fd163b78
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2023-09-27 09:57:27 -0600

    add bits for working initial discord bot loop
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+#!/usr/bin/env python
 """main entrypoint for the bot"""
 
-#!/usr/bin/env python
 import sys
 from logging import error
-from multiprocessing import Pool
 from fire import Fire
-from packages.config.config import init, services
+from packages.config.config import init
 from packages.services.services import clients
 from packages.errors.errors import ConfigError
 
@@ -14,12 +13,10 @@ def Run(config:str=".snowball.conf") -> None:
     rolling was posting increasing counting messages in a discord channel"""
     try:
         init(config)
-        with Pool(2) as pool:
-            pool.apply_async(lambda service: clients[service](), services.keys())
     except ConfigError as cerr:
         error(cerr)
         sys.exit(1)
-
+    clients["discord"]()
 
 if __name__ == "__main__":
     Fire(Run)
