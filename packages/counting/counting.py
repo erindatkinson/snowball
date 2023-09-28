359da95716488c1188a37bb2450c570e8efc4af1
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2023-09-28 10:10:11 -0600

    Add initial counting logic (#3)
    * pushing what I got for counting so far
    
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
    
    * pretty much have the parsing done
    
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
    
    * hook in state!
    
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
    
    * remove temp text
    
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
    
    * remove other temp text
    
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>

--- packages/counting/counting.py
+++ packages/counting/counting.py
@@ -1,11 +1,38 @@
 """module maaging counting state"""
+from subprocess import run, CalledProcessError
+from logging import debug
+from re import search
 
-def check_next_count(message:str) -> bool:
-    """check if the message is valid for the next expected count"""
-    return isinstance(message, str)
+def parse_message(message:str) -> tuple[int, bool]:
+    """parses message and uses linux dc to calculate math
+    returns a tuple of the value and a bool of if the message was countable"""
 
-def increment_count()->None:
-    """update the state of the expected count to the next iteration"""
+    # try to see if the number is just a number
+    try:
+        count = int(message)
+        return (count, True)
+    except ValueError:
+        debug("message not wholly integer")
 
-def reset_count()->None:
-    """reset the state of the expected count back to the beginning"""
+    # check if the first character is a digit if not, return not countable
+    try:
+        int(message[0])
+    except ValueError:
+        return (-1, False)
+
+    # check to see if there's like text in the message after some math
+    pattern = r"[a-zA-Z]"
+    search_match = search(pattern, message)
+    if search_match is not None:
+        math = message[:search_match.start()]
+        message = math
+    
+    # shim out to linux desk calculator to see if message is postfix math
+    try:
+        data = run(["dc", "-e", message + " p"], capture_output=True, check=True)
+        count = int(data.stdout)
+        return (count, True)
+    except ValueError:
+        return (-1, True)
+    except CalledProcessError:
+        return (-1, True)
