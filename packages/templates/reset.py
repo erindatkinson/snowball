9c5f233c42be85632d0c2143b9a7ed1535b1ffdf
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2023-12-11 16:04:45 -0700

    fix typo
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>

--- packages/templates/reset.py
+++ packages/templates/reset.py
@@ -1,4 +1,4 @@
 """module for reset templates"""
 
 reset_string = """{emoji_string} The cycle begins anew.
-The correct count was {count} but I recieved {this_count}"""
+The correct count was {count} but I received {this_count}"""
